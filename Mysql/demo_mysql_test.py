9d8e91f213d5c2bc52e870344c1eda9f970ae9f6
DuXin <duxin@ysstech.com>
2023-02-20 17:47:24 +0800

    2.20

--- Mysql/demo_mysql_test.py
+++ Mysql/demo_mysql_test.py
@@ -1,18 +1,11 @@
-#!/usr/bin/python3
-
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host="localhost",  # 数据库主机地址
-    user="root",  # 数据库用户名
-    passwd="123456"  # 数据库密码
+    host="localhost",
+    user="root",
+    passwd="123456"
 )
-
-print(mydb)
 
 mycursor = mydb.cursor()
 
-# 打印有哪些数据库
-mycursor.execute("SHOW DATABASES")
-for x in mycursor:
-  print(x)
+mycursor.execute("CREATE DATABASE runoob_db")
