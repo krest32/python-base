9446ebf20fb811b2d007bfb4a96a5c7e075a7d1f
krest <18635572970@163.com>
2023-02-21 20:26:07 +0800

    2.21

--- Mysql/demo_mysql_test.py
+++ Mysql/demo_mysql_test.py
@@ -7,7 +7,7 @@ import pymysql
 
 db = pymysql.connect(host='localhost',
                      user='root',
-                     password='123456',
+                     password='Bob.123456',
                      database='demo')
 
 
