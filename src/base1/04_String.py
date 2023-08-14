72782f01ea18c16455339a4951fb2dcc7b629300
krest <18635572970@163.com>
2023-08-14 20:55:47 +0800

    8.14

--- src/base1/04_String.py
+++ src/base1/04_String.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+    print('Ru\noob')  # 换行打印
+    print(r'Ru\noob')  # 将\n无效化
 
-print('Ru\noob')  # 换行打印
-
-print(r'Ru\noob')   # 将\n无效化
--- src/base1/23_exception1.py
+++ src/base1/23_exception1.py
@@ -3,6 +3,7 @@
 class MyClass:
     """一个简单的类实例"""
     i = 12345
+
     def f(self):
         return 'hello world'
 
